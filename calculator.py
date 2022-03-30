4aed13f0685ff77af0a02382c63fdbbd5a5696e9
agira <imagirina@gmail.com>
2022-03-30 15:51:07 -0700

    Added check length

--- calculator.py
+++ calculator.py
@@ -6,37 +6,41 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
 while True:
 
     expr = input("Enter an expression > ").split(" ")
+    #print(len(expr))
     
     operator = expr[0]
 
     if operator == "q":
         break
-
-    num1 = expr[1]
-    num2 = expr[2]
-    
-    if (num1.isdigit() and num2.isdigit()):
-
-        num1 = float(num1)
-        num2 = float(num2)
-
-        if operator == "+":
-            print(add(num1, num2))
-        elif operator == "-":
-            print(subtract(num1, num2))
-        elif operator == "*":
-            print(multiply(num1, num2))
-        elif operator == "/":
-            print(divide(num1, num2))
-        elif operator == "square":
-            print(square(num1, num2))
-        elif operator == "cube":
-            print(cube(num1, num2))
-        elif operator == "power":
-            print(power(num1, num2))   
-        elif operator == "mod":
-            print(mod(num1, num2))                             
-        else:
-            print("Operator does not exist. Try again. ")
+    elif len(expr) != 3:
+        print("Not enough arguments.")
     else:
-        print("Those are not numbers. Try again.")
+
+        num1 = expr[1]
+        num2 = expr[2]
+        
+        if num1.isdigit() and num2.isdigit():
+
+            num1 = float(num1)
+            num2 = float(num2)
+
+            if operator == "+":
+                print(add(num1, num2))
+            elif operator == "-":
+                print(subtract(num1, num2))
+            elif operator == "*":
+                print(multiply(num1, num2))
+            elif operator == "/":
+                print(divide(num1, num2))
+            elif operator == "square":
+                print(square(num1, num2))
+            elif operator == "cube":
+                print(cube(num1, num2))
+            elif operator == "power":
+                print(power(num1, num2))   
+            elif operator == "mod":
+                print(mod(num1, num2))                             
+            else:
+                print("Operator does not exist. Try again. ")
+        else:
+            print("Those are not numbers. Try again.")

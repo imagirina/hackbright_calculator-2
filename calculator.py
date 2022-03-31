2f55108dbac92a70a0f59a6242965795fed716dc
agira <imagirina@gmail.com>
2022-03-30 19:16:47 -0700

    Simplified if/else, enhanced Command Line output

--- calculator.py
+++ calculator.py
@@ -3,44 +3,52 @@
 from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
+print("""
+Please enter an expression.
+Use (= - * / pow mod square cube) followed by two digits.
+Press \"q\" to exit.
+""")
+
 while True:
 
-    expr = input("Enter an expression > ").split(" ")
-    #print(len(expr))
-    
+    expr = input("\texpression: ").split(" ")        
     operator = expr[0]
 
     if operator == "q":
         break
     elif len(expr) != 3:
-        print("Not enough arguments.")
-    else:
+        print("\tNot enough or more than needed arguments.\n")
+        continue 
 
-        num1 = expr[1]
-        num2 = expr[2]
-        
-        if num1.isdigit() and num2.isdigit():
+    num1 = expr[1]
+    num2 = expr[2]
+    result = None
 
-            num1 = float(num1)
-            num2 = float(num2)
+    if num1.isdigit() and num2.isdigit():
 
-            if operator == "+":
-                print(add(num1, num2))
-            elif operator == "-":
-                print(subtract(num1, num2))
-            elif operator == "*":
-                print(multiply(num1, num2))
-            elif operator == "/":
-                print(divide(num1, num2))
-            elif operator == "square":
-                print(square(num1, num2))
-            elif operator == "cube":
-                print(cube(num1, num2))
-            elif operator == "power":
-                print(power(num1, num2))   
-            elif operator == "mod":
-                print(mod(num1, num2))                             
-            else:
-                print("Operator does not exist. Try again. ")
+        num1 = float(num1)
+        num2 = float(num2)
+
+        if operator == "+":
+            result = add(num1, num2)
+        elif operator == "-":
+            result = subtract(num1, num2)
+        elif operator == "*":
+            result = multiply(num1, num2)
+        elif operator == "/":
+            result = divide(num1, num2)
+        elif operator == "square":
+            result = square(num1, num2)
+        elif operator == "cube":
+            result = cube(num1, num2)
+        elif operator == "pow":
+            result = power(num1, num2)
+        elif operator == "mod":
+            result = mod(num1, num2)
         else:
-            print("Those are not numbers. Try again.")
+            print("\tPlease enter an operator followed by two integers.\n")
+        
+        print(f"\tanswer: {result}\n")        
+    else:
+        print("\tThose are not numbers. Try again.\n")    
+    
